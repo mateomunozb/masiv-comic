7e69b46b610c4526c965a734ec2bbf45355fe16d
root <mateomunozb@gmail.com>
2020-11-28 17:00:33 -0500

    Pantalla de comic completa

--- src/components/Comic.js
+++ src/components/Comic.js
@@ -6,34 +6,51 @@ const Comic = () => {
   return (
     <div className='comiccover'>
       <div className='cover'>
-        <h1>xkcd</h1>
+        <a href='/'>
+          <h1>xkcd</h1>
+        </a>
         <div>
           <p>A webcomic of romance, sarcasm, math, and language.</p>
-          <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</span>
         </div>
       </div>
       <div className='title'>
         <h1>Life Before the Pandemic</h1>
       </div>
       <div className='image'>
-        <img src='https://imgs.xkcd.com/comics/life_before_the_pandemic.png' alt='' />
+        <img
+          src='https://imgs.xkcd.com/comics/life_before_the_pandemic.png'
+          alt=''
+        />
       </div>
       <div className='number'>
         <div className='date'>
           <h1>Num. 20528</h1>
           <h2>Day: 27 Month: 11 Year: 2020 </h2>
         </div>
-        <a href='http://www.freepik.com'>
+        <a href='http://www.freepik.com' target='_blank' rel='noreferrer'>
           <img src={Barcode} alt='' />
         </a>
       </div>
       <div className='description'>
-        <p>Our retina display features hundreds of pixels per inch in the central fovea region.</p>
+        <p>
+          Our retina display features hundreds of pixels per inch in the central
+          fovea region.
+        </p>
         <div className='refresh'>
-          <h1>REFRESH!</h1>
-          <span>Or go to xkcd</span>
+          <a href='/'>
+            <h1>REFRESH!</h1>
+          </a>
+          <a
+            href='https://xkcd.com/'
+            target='
+            _blank'
+            rel='noreferrer'
+          >
+            <span>Or go to xkcd</span>
+          </a>
         </div>
       </div>
+      <div className='foot'></div>
     </div>
   )
 }
